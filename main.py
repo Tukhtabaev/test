206624b1e56a074a00757f8bb12aeba01e2784b7
Tukhtabaev S <sodqz2724@icloud.com>
2022-01-10 19:29:40 +0500

    print qowdim

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # git status - kuzatilmayotgan faylni aniqlaydi
+
+# ls -la --- skritiy faylni topiw
+
+# git add . -- qaysi faylni kuzatmoqci bolsa qowiw ucun
+#  git log -p -(raqam) --- oxirgi save larni korish
+
+
+print('hello')
+# test
+a = 5
+print(a)
