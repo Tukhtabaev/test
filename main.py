9525bd08b923978a40c94404e0f2334ae9405a75
Tukhtabaev S <sodqz2724@icloud.com>
2022-01-10 20:18:22 +0500

    ddd

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # git add . -- qaysi faylni kuzatmoqci bolsa qowiw ucun
 #  git log -p -(raqam) --- oxirgi save larni korish
 
-
+# git merge dev ---- qowimcha vetkadan master vetkaga malumot qowiw
 print('hello')
 # test
 a = 5
